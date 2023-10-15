9d53a4be4c0c008d0bb1c61862e453cb3faa5b7a
Lumic-code <wisdomlumic0368@gmail.com>
2023-10-15 16:11:03 +0100

    CRUD operations done

---
+++ DynamoStudentManager/Controllers/StudentsController.cs
@@ -0,0 +1,61 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using DynamoStudentManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoStudentManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDynamoDBContext _context;
+
+        public StudentsController(IDynamoDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetById(int studentId)
+        {
+            var student = await _context.LoadAsync<Student>(studentId);
+            if (student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpGet("get-all-students")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var studeents = await _context.ScanAsync<Student>(default).GetRemainingAsync();
+            return Ok(studeents);
+        }
+
+        [HttpPost("add-student")]
+        public async Task<IActionResult> CreateStudent(Student studentRequest)
+        {
+            var student = await _context.LoadAsync<Student>(studentRequest.Id);
+            if (student != null) return BadRequest($"Student with Id {studentRequest.Id} already exists.");
+            await _context.SaveAsync(studentRequest);
+            return Ok(studentRequest);
+        }
+
+        [HttpDelete("{studentId}")]
+        public async Task<IActionResult> DeleteStudent(int studentId)
+        {
+            var student = await _context.LoadAsync<Student>(studentId);
+            if (student == null) return NotFound();
+            await _context.DeleteAsync(student);
+            return Ok($"Student with Id {studentId} successfully deleted.");
+        }
+
+        [HttpPut("update-student-details")]
+        public async Task<IActionResult> UpdateStudent(Student studentRequest)
+        {
+            var student = await _context.LoadAsync<Student>(studentRequest.Id);
+            if (student == null) return NotFound();
+            await _context.SaveAsync(studentRequest);
+            return Ok(studentRequest);
+        }
+    }
+}
--- DynamoStudentManager/Program.cs
+++ DynamoStudentManager/Program.cs
@@ -1,3 +1,6 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var awsOptions = builder.Configuration.GetAWSOptions();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
